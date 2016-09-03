7d7e40643fe396932ed0c4a24a5c87fb72b3c7b9
Danillo Nunes <git@danillonunes.net>
2016-09-03 18:47:37 -0300

    Initial commit :)

---
+++ prm.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * @package PRM
+ */
+/*
+Plugin Name: PRM
+Plugin URI: https://wordpress.org/plugins/orghunter
+Description: PRM: The Patrão Relationship Management!
+Version: 0.1
+Author: Danillo Nunes
+Author URI: http://danillonunes.com
+License: GPLv2 or later
+Text Domain: prm
+*/
+
+/*
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
---
+++ readme.txt
@@ -0,0 +1,10 @@
+=== PRM ===
+Contributors: danillonunes
+Tags: patron, donation, donator, management
+Requires at least: 3.7
+Tested up to: 4.6
+Stable tag: 0.1
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+PRM: The Patrão Relationship Management!
